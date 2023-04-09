d7c5db73c1df5a941fc8d4251cd03b21070ada0e
Maple_of_Ten <75062138+shifengzhicheng@users.noreply.github.com>
2023-04-09 14:54:51 +0800

    Add files via upload

---
+++ Top_module.m
@@ -0,0 +1,31 @@
+%% 项目的顶层模块，用于实现整个项目的仿真流程
+%% 读取文件，预处理阶段
+filename = 'dbmixer.sp';
+[RLCName,RLCN1,RLCN2,RLCarg1,...
+    SourceName,SourceN1,SourceN2,...
+    Sourcetype,SourceDcValue,SourceAcValue,...
+    SourceFreq,SourcePhase,...
+    MOSName,MOSN1,MOSN2,MOSN3,...
+    MOStype,MOSW,MOSL,...
+    MOSMODEL,PLOT,SPICEOperation]=parse_netlist(filename);
+%% 根据读到的操作选择执行任务的分支
+switch SPICEOperation{1}{1}
+    case '.hb'
+        % 这里进入AC分析
+        % 需要时间步长，AC频率
+    case '.trans'
+        % 设置判断解收敛的标识
+        Error = 1e-6;
+        timestep = 1e-3;
+        % 到这里需要进行瞬态仿真
+        % 瞬态仿真需要时间步长
+        x=caculateDC(DCnetlist,Error);
+        plotport=portMapping(PLOT);
+        [timeseries,Values]=trans(plotport,x,transnetlist);
+        plot(timeseries,Values);
+    case {'.dc','.DC'}
+        Error = 1e-6;
+        % 到这里需要DC电路网表
+        x=caculateDC(DCnetlist,Error);
+        plotport=portMapping(PLOT);
+end
--- parse_netlist.m
+++ parse_netlist.m
@@ -1,1 +1,90 @@
-%% 这个文档将解析网表并将网表的数据提取出来
+% 此函数由郑志宇完成，读取网表文件并提取所有的有效信息
+%% 函数主体
+% 这个函数获得的数据来自网表
+function [RLCName,RLCN1,RLCN2,RLCarg1,...
+    SourceName,SourceN1,SourceN2,...
+    Sourcetype,SourceDcValue,SourceAcValue,...
+    SourceFreq,SourcePhase,...
+    MOSName,MOSN1,MOSN2,MOSN3,...
+    MOStype,MOSW,MOSL,...
+    MOSMODEL,PLOT,SPICEOperation]=parse_netlist(filename)
+% 读取网表文件
+fid = fopen(filename, 'r');
+Count=0;
+MCount=0;
+SourceCount=0;
+MOSMODELCount=0;
+PlotCount=0;
+OperationCount=0;
+while ~feof(fid)
+    % 读取一行
+    line = fgets(fid);
+    % 匹配开头的关键字母
+    tokens_Device = regexp(line, '^(\w+)', 'tokens');
+    % 匹配MODEL
+    tokens_MODEL = regexp(line, '^(\.MODEL)', 'tokens');
+    % 匹配作图节点数据
+    tokens_Plot = regexp(line, '^(\.plot)', 'tokens', 'ignorecase');
+    % 匹配操作
+    tokens_Operation = regexp(line, '^[(\.hb)(\.trans)]', 'tokens', 'ignorecase');
+    if ~isempty(tokens_Device)
+        % 提取关键字符
+        keyword = tokens_Device{1}{1}(1);
+        Device = strsplit(strtrim(line));
+        switch keyword
+            case {'R','L','C'}
+                % RLC处理
+                Count = Count + 1;
+                [RLCName{Count},RLCN1{Count},RLCN2{Count},RLCarg1{Count}]=Device{:};
+            case {'V','I'}
+                % 直流交流源处理
+                SourceCount = SourceCount + 1;
+                % 这里要对交流直流进行讨论处理
+                Sourcetype{SourceCount}=Device(4);
+                switch Sourcetype{SourceCount}{1}
+                    case 'SIN'
+                        [SourceName{SourceCount},SourceN1{SourceCount},SourceN2{SourceCount},...
+                            Sourcetype{SourceCount},SourceDcValue{SourceCount},SourceAcValue{SourceCount},...
+                            SourceFreq{SourceCount},SourcePhase{SourceCount}]=Device{:};
+
+                    case {'DC','dc'}
+                        [SourceName{SourceCount},SourceN1{SourceCount},SourceN2{SourceCount},...
+                            Sourcetype{SourceCount},SourceDcValue{SourceCount}]=Device{:};
+                end
+            case {'M'}
+                % MOS管处理
+                MCount = MCount+1;
+                [MOSName{MCount},MOSN1{MCount},MOSN2{MCount},MOSN3{MCount}...
+                    MOStype{MCount},MOSW{MCount},MOSL{MCount}]=Device{:};
+        end
+    end
+    if ~isempty(tokens_MODEL)
+        % 匹配的正则表达式
+        expr = ['\.MODEL\s+(\d+)\s+VT\s+([\+\-]?\d*\.?\d+(?:[eE][\+\-]?\d+)?)' ...
+            '\s+MU\s+([\+\-]?\d*\.?\d+(?:[eE][\+\-]?\d+)?)\s+COX\s+([\+\-]?\d*\.?\d+(?:[eE][\+\-]?\d+)?)\s+' ...
+            'LAMBDA\s+([\+\-]?\d*\.?\d+(?:[eE][\+\-]?\d+)?)\s+CJ0\s+([\+\-]?\d*\.?\d+(?:[eE][\+\-]?\d+)?)'];
+        % 在这里已经匹配到了模型数据
+        % 按照标准的格式进行模型的数据赋值
+        tokens = regexp(line, expr, 'tokens');
+        MOSMODELCount = MOSMODELCount + 1;
+        MOSMODEL{MOSMODELCount}=[str2double(tokens{1}{1}); ...
+            str2double(tokens{1}{2}); ...
+            str2double(tokens{1}{3}); ...
+            str2double(tokens{1}{4}); ...
+            str2double(tokens{1}{5}); ...
+            str2double(tokens{1}{6})];
+    end
+    if ~isempty(tokens_Plot)
+        % 在这里已经匹配到了作图的节点
+        PlotCount = PlotCount + 1;
+        PLOT{PlotCount}=strsplit(strtrim(line));
+    end
+    if ~isempty(tokens_Operation)
+        % 在这里已经匹配到了操作的名称
+        OperationCount = OperationCount + 1;
+        SPICEOperation{OperationCount}=strsplit(strtrim(line));
+    end
+end
+% 关闭文件
+fclose(fid);
+end
