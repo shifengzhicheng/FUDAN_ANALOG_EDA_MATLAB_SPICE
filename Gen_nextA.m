5c6f347ac20efcfe73522e75f0fb5556e907671e
Maple_of_Ten <75062138+shifengzhicheng@users.noreply.github.com>
2023-04-16 17:27:15 +0800

    Add files via upload

---
+++ Gen_nextA.m
@@ -0,0 +1,57 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Gen_nextA%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Gen_nextA生成下一轮A和b
+function [A, b] = Gen_nextA(pureA, pureb, Name, N1, N2, dependence, Value)
+CellCount = length(Name);% 元件数量
+% 初始化矩阵 因为建立MNA时先暂时引入0节点
+A = [zeros(size(pureA,1),1), pureA];
+A = [zeros(1,size(A, 2)); A];
+b = [0; pureb];
+%x=compose('v_%d',(0:nodeNums)'); %不必要了
+
+%% 只处理MOS相关伴随器件
+for i=1:CellCount
+    CellName=Name{i};
+    % 考虑节点地，方程要往右往下移动一个单位
+    pNum1=N1(i)+1;
+    pNum2=N2(i)+1;
+    switch CellName(1)
+        case 'R'
+            if CellName(2) == 'M'
+                %% 在电路上贴MOS得到的伴随电阻
+                cpValue=Value(i);
+                % 方程贴电阻
+                A(pNum1,pNum1)= A(pNum1,pNum1)+1/cpValue;
+                A(pNum1,pNum2)= A(pNum1,pNum2)-1/cpValue;
+                A(pNum2,pNum1)= A(pNum2,pNum1)-1/cpValue;
+                A(pNum2,pNum2)= A(pNum2,pNum2)+1/cpValue;
+            end
+        case 'I'
+            if CellName(2) == 'M'
+                %% 在电路上贴MOS得到的伴随电流源
+                cpValue=Value(i);
+                % 节点的净流出与净流入电流
+                b(pNum1)=b(pNum1)-cpValue;
+                b(pNum2)=b(pNum2)+cpValue;
+            end
+        case 'G'
+            %% MOS得到的伴随压控电流源 (VCCS) - G
+            % 控制端口与增益
+            cpNum1=dependence{i}(1)+1;
+            cpNum2=dependence{i}(2)+1;
+            cpValue=Value(i);
+            % 压控电流源 (VCCS) - G
+            if CellName(2) == 'M'
+                % 电压控制的电流源没有引入新的变量
+                % 会给一些端口引入电流
+                A(pNum1,cpNum1)= A(pNum1,cpNum1) + cpValue;
+                A(pNum1,cpNum2)= A(pNum1,cpNum2) - cpValue;
+                A(pNum2,cpNum1)= A(pNum2,cpNum1) - cpValue;
+                A(pNum2,cpNum2)= A(pNum2,cpNum2) + cpValue;
+            end
+    end
+end
+A(1,:)=[];
+A(:,1)=[];
+b(1)=[];
+end
---
+++ Mos_Calculator.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Mos_Calculater%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 根据牛顿迭代公式得到MOS伴随器件信息
+function [Ikk,GMk,GDSk]=Mos_Calculator(VDSk,VGSk,Mosarg,W,L)
+    Mosarg = cell2mat(Mosarg);
+    Type = power(-1,Mosarg(1,1)); %-1是PMOS,1是NMOS
+    Vth = Mosarg(2,1);
+    MU = Mosarg(3,1);
+    COX = Mosarg(4,1);
+    LAMBDA = Mosarg(5,1);
+%根据P、NMOS不同Vth有正负，迭代公式完全是ids推导，PMOS注意也是ids方向
+    if VGSk*Type < Vth*Type           %截止区
+%       Ik = 0;
+        GMk = 0;
+        GDSk = 0;
+        Ikk = 0;
+    elseif (VGSk-VDSk)*Type > Vth*Type  %线性区
+        Ik = Type*MU*COX*(W/L)*(VGSk-Vth-(1/2)*VDSk)*VDSk;
+        GMk = Type*MU*COX*(W/L)*VDSk;
+        GDSk = Type*MU*COX*(W/L)*(VGSk-Vth-VDSk);
+        Ikk = Ik - GMk*VGSk -GDSk*VDSk;
+    else                    %饱和区
+        Ik = Type*(1/2)*MU*COX*(W/L)*(VGSk-Vth)*(VGSk-Vth)*(1+LAMBDA*VDSk*Type);
+        GMk = Type*MU*COX*(W/L)*(VGSk-Vth)*(1+LAMBDA*VDSk*Type);
+        GDSk = 1/2*MU*COX*(W/L)*(VGSk-Vth)*(VGSk-Vth)*LAMBDA;
+        Ikk = Ik - GMk*VGSk -GDSk*VDSk;
+    end
+
+end
