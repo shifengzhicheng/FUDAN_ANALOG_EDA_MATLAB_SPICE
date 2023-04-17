4ccaa93d4025f89352bf7cd3552045d45583917f
Maple_of_Ten <1806471942@qq.com>
2023-04-17 15:29:01 +0800

    ValueCalc

--- ValueCalc.m
+++ ValueCalc.m
@@ -1,18 +1,18 @@
 %% 根据device以及端点从解中得到电流或者电压
-function [Obj,Values] = ValueCalc(plotnv,plotcurrent,x,x_0)
+function [Obj,Values] = ValueCalc(plotnv,plotcurrent,x,x_0,Node_Map)
 % 画图对象的总数量
 tsize = size(plotnv)+size(plotcurrent);
 % 初始化
 Obj = cell(tsize);
 Values = zeros(tsize);
     for i=1:size(plotnv)
-        Obj(i) = invtrans(plotnv);
+        Obj(i) = Node_Map(plotnv);
         % 基本逻辑是在解出来的结果中找到对应的节点然后得到其电压
         Values(i) = x(plotnv);
     end
     for j= i+1:tsize
         dname = plotcurrent{j-i}{1};
-        Obj(j) = [dname ': ' invtrans(plotcurrent{j-i}{2})];
+        Obj(j) = [dname ': ' Node_Map(plotcurrent{j-i}{2})];
         switch dname(1)
             case M
                 % 基本逻辑是在解出来的结果中找到对应的器件然后得到其电流
--- portMapping.m
+++ portMapping.m
@@ -1,12 +1,12 @@
 %% 端口映射函数主体，将PLOT代码部分解析并生成所需要的
-function [plotnv,plotCurrent] = portMapping(PLOT)
+function [plotnv,plotCurrent] = portMapping(PLOT,Node_Map)
     plotnum=size(PLOT,2);
     for i=1:plotnum
         switch lower(PLOT{i}{1})
             case '.plotnv'
-                plotnv=[plotnv, tran(PLOT{i}{2})];
+                plotnv=[plotnv, find(Node_Map,PLOT{i}{2})];
             case '.plotni'
-                plotCurrent={plotCurrent, {PLOT{i}{2}, tran(PLOT{i}{3})}};
+                plotCurrent={plotCurrent, {PLOT{i}{2}, find(Node_Map,PLOT{i}{3})}};
         end
     end
 end
