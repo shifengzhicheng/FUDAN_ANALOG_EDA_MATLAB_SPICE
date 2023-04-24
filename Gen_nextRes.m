15547e56e14e9423476d1c6f45fd023420c7f169
Facunder <127582736+Facunder@users.noreply.github.com>
2023-04-24 17:34:38 +0800

    Update Gen_nextRes.m
    参数加入MOSID
    Mos_Calculator调用方式中参数MOSMODEL(:, MosType(mosCount))改为MOSMODEL(:, MOSID(mosCount))

--- Gen_nextRes.m
+++ Gen_nextRes.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Gen_nextRes%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [zc, dependence, Value] = Gen_nextRes(MOSMODEL, Mostype, MOSW, MOSL, mosNum, mosNodeMat, MOSLine, A0, b0, Name, N1, N2, dependence, Value, zp)
+function [zc, dependence, Value] = Gen_nextRes(MOSMODEL, Mostype, MOSW, MOSL, mosNum, mosNodeMat, MOSLine, MOSID, A0, b0, Name, N1, N2, dependence, Value, zp)
 %已经得到了按顺序的每个MOS管的三端的节点序号，带入x(z)p结果得到上轮具体三端电压
     for mosCount = 1 : mosNum   % 1个MOS衍生出的3个伴随器件1组
 %% 可能出现源漏端交换的情况，我们固定初始GDS的物理位置，源漏交换只体现在伴随器件的数值正负上
@@ -13,7 +13,7 @@ function [zc, dependence, Value] = Gen_nextRes(MOSMODEL, Mostype, MOSW, MOSL, mo
             vds = vs - vd;
             vgs = vg - vd;
             %用上一轮x(z)p结果的到的三端电压计算得到新的伴随器件参数(MOS合法判断在Mos_Calculator中)
-            [nextIeq, nextGM, nextGDS] = Mos_Calculator(vds, vgs,  MOSMODEL(:, Mostype(mosCount)), MOSW(mosCount), MOSL(mosCount));
+            [nextIeq, nextGM, nextGDS] = Mos_Calculator(vds, vgs,  MOSMODEL(:, MOSID(mosCount)), MOSW(mosCount), MOSL(mosCount));
             nextIeq = -nextIeq;
             nextGM = -nextGM;
             %源漏交互换后GM的控制电压端口也要改变为原来的栅漏端 - 原GM的第二个控制端由S改D
@@ -22,7 +22,7 @@ function [zc, dependence, Value] = Gen_nextRes(MOSMODEL, Mostype, MOSW, MOSL, mo
             %正常情况
             vds = vd - vs;
             vgs = vg - vs;
-            [nextIeq, nextGM, nextGDS] = Mos_Calculator(vds, vgs, MOSMODEL(:, Mostype(mosCount)), MOSW(mosCount), MOSL(mosCount));
+            [nextIeq, nextGM, nextGDS] = Mos_Calculator(vds, vgs, MOSMODEL(:, MOSID(mosCount)), MOSW(mosCount), MOSL(mosCount));
             %源漏换回来，正常vgs控制
            dependence{MOSLine + 3*mosCount - 2}(2) = mosNodeMat(mosCount, 3);
         end
