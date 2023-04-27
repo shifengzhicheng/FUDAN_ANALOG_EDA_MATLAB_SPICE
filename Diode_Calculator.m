3b2aaf8ecdf6f7a239a69638671562deee88d188
Facunder <127582736+Facunder@users.noreply.github.com>
2023-04-27 15:29:24 +0800

    diode迭代模型值计算

---
+++ Diode_Calculator.m
@@ -0,0 +1,8 @@
+%根据二极管反向饱和电流Is，本轮两端正向电压Vpn，温度(默认27℃-300K)
+%得到伴随器件值Gdk = 1/Rk, Ieqk
+function [Gdk, Ieqk] = Diode_Calculator(Vpn, Is, T)
+    Vt = 8.6173e-5 * (273.15 + T);
+    Gdk = (Is / Vt) * exp(Vpn / Vt);
+    Icur = Is * (exp(Vpn / Vt) - 1);
+    Ieqk = Icur - Gdk * Vpn;
+end
