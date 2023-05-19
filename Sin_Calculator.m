58c2587c4bddae97e24572a277420d07eac888c3
Facunder <127582736+Facunder@users.noreply.github.com>
2023-05-19 23:07:03 +0800

    Update Sin_Calculator.m

--- Sin_Calculator.m
+++ Sin_Calculator.m
@@ -1,5 +1,5 @@
 %计算正弦源瞬态值 需要初相和时间
 function [Vt] = Sin_Calculator(Vdc, Vac, Freq, t, Phase)
-    CurPhase = (t*Freq + Phase/360)*2*pi;
-    Vt = Vdc + Vac * sin(CurPhase);
+    CurPhase = (t.*Freq + Phase./360).*2.*pi;
+    Vt = Vdc + Vac .* sin(CurPhase);
 end
