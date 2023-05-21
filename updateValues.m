9ec9106e5a2c60c2122f1daee8ac656dda356f5d
Maple_of_Ten <1806471942@qq.com>
2023-05-21 22:57:04 +0800

    更新ValueCalc

--- updateValues.m
+++ updateValues.m
@@ -13,19 +13,19 @@ Values((1 : nvNum), :) = DCresData(plotnv,:);
 %% 节点电流信息
 %mosIndexInValues\mosIndexInmosCurrents都是列向量 - 更改Values结果里要的mos管电流
 if(~isempty(mosIndexInValues))
-    Values(mosIndexInValues, :) = Values(mosIndexInValues, :) .* mosCurrents(mosIndexInmosCurrents);
+    Values(mosIndexInValues, :) = Values(mosIndexInValues, :) .* mosCurrents(mosIndexInmosCurrents,:);
 end
 %更改Values结果里要的diode管电流
 if(~isempty(dioIndexInValues))
-    Values(dioIndexInValues, :) = Values(dioIndexInValues, :) .* diodeCurrents(dioIndexIndiodeCurrents);
+    Values(dioIndexInValues, :) = Values(dioIndexInValues, :) .* diodeCurrents(dioIndexIndiodeCurrents,:);
 end
 %更改Values结果里要的电源电压V
 if(~isempty(VIndexInValues))
-    Values(VIndexInValues, :) = Values(VIndexInValues, :) .* DCresData(VIndexInDCres);
+    Values(VIndexInValues, :) = Values(VIndexInValues, :) .* DCresData(VIndexInDCres,:);
 end
 %更改Values结果里要的电源电流Is
 if(~isempty(IIndexInValues))
-    Values(IIndexInValues, :) = Values(IIndexInValues, :) .* LinerValue(IIndexInValue);
+    Values(IIndexInValues, :) = Values(IIndexInValues, :) .* LinerValue(IIndexInValue,:);
 end
 %更改Values结果里要的电阻电流
 if(~isempty(RIndexInValues))
